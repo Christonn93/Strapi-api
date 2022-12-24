b78055dc93875b8b508175f4b2b08bbbcb8a15ed
Christopher Tønnesland <46559129+Christonn93@users.noreply.github.com>
2022-12-24 11:02:39 +0100

    adding files

---
+++ src/api/url-storage/controllers/url-storage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * url-storage controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::url-storage.url-storage');
---
+++ src/api/url-storage/routes/url-storage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * url-storage router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::url-storage.url-storage');
